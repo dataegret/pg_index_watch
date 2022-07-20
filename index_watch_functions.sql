c5106f687a513276253a06022ea7944db01ad329
Maksym Boguk <maxim.boguk@dataegret.com>
2022-07-20 22:10:37 +0200

    skip current_database in index_watch.periodic()

--- index_watch_functions.sql
+++ index_watch_functions.sql
@@ -780,7 +780,7 @@ BEGIN
       WHERE 
         NOT datistemplate 
         AND datallowconn 
-        --AND datname<>current_database()
+        AND datname<>current_database()
         AND index_watch.get_setting(datname, NULL, NULL, NULL, 'skip')::boolean IS DISTINCT FROM TRUE
       ORDER BY datname
     LOOP
